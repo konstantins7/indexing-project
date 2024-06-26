67460961fb5065a1e4c1da8d033444189c341fca
konstantins7 <saldinkostya97@gmail.com>
2024-06-26 19:13:28 +0500

    Update indexer.py

--- scripts/indexer.py
+++ scripts/indexer.py
@@ -103,6 +103,9 @@ def fetch_sitemap_links(sitemap_url):
     return []
 
 def send_telegram_message(message):
+    if not TELEGRAM_TOKEN:
+        print("Missing Telegram token, skipping sending message.")
+        return None
     print(f"Sending Telegram message: {message}")
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     data = {
